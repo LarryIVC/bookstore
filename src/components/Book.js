5be457a7cf0daba43511a31cbae9787d50f1bcc5
Larry Villegas <larry_villegas@hotmail.com>
2023-05-17 01:59:15 -0400

    Update Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,34 +1,53 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
 const Book = ({
-  category, title, author, progress, chapter,
-}) => (
-  <>
-    <div>
+  id,
+  category,
+  title,
+  author,
+  progress,
+  chapter,
+}) => {
+  const dispatch = useDispatch();
+  return (
+    <>
       <div>
-        <p>{category}</p>
-        <h2>{title}</h2>
-        <p>{author}</p>
-        <button type="button">Comments</button>
-        <button type="button">Remove</button>
-        <button type="button">Edit</button>
-      </div>
-      <div>
-        <p>{progress}</p>
-        <p>Completed</p>
-      </div>
+        <div>
+          <p>{category}</p>
+          <h2>{title}</h2>
+          <p>{author}</p>
+          <button
+            type="button"
+          >
+            Comments
+          </button>
+          <button
+            type="button"
+            onClick={() => dispatch(removeBook(id))}
+          >
+            Remove
+          </button>
+          <button type="button">Edit</button>
+        </div>
+        <div>
+          <p>{progress}</p>
+          <p>Completed</p>
+        </div>
 
-      <div>
-        <h3>CURRENT CHAPTER</h3>
-        <p>{chapter}</p>
-        <button type="button">UPDATE PROGRESS</button>
+        <div>
+          <h3>CURRENT CHAPTER</h3>
+          <p>{chapter}</p>
+          <button type="button">UPDATE PROGRESS</button>
+        </div>
       </div>
-    </div>
-  </>
-
-);
+    </>
+  );
+};
 
 Book.propTypes = {
+  id: PropTypes.number.isRequired,
   category: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
