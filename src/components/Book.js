3cff3112b11881f76c792876a314a76d4f04e112
Larry Villegas <larry_villegas@hotmail.com>
2023-05-19 20:03:49 -0400

    Update Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,28 +1,30 @@
+/* eslint-disable camelcase */
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/booksSlice';
+import '../css/Book.css';
 
 const Book = ({ data }) => {
   const {
-    itemId, category, title, author,
+    item_id,
+    category,
+    title,
+    author,
   } = data;
   const dispatch = useDispatch();
+  const handleRemoveBook = () => {
+    dispatch(removeBook(item_id));
+  };
+
   return (
     <>
-      <div>
+      <div className="book">
         <div>
           <p>{category}</p>
           <h2>{title}</h2>
           <p>{author}</p>
-          <button
-            type="button"
-          >
-            Comments
-          </button>
-          <button
-            type="button"
-            onClick={() => dispatch(removeBook(itemId))}
-          >
+          <button type="button">Comments</button>
+          <button type="button" onClick={() => handleRemoveBook(item_id)}>
             Remove
           </button>
           <button type="button">Edit</button>
@@ -44,7 +46,7 @@ const Book = ({ data }) => {
 
 Book.propTypes = {
   data: PropTypes.shape({
-    itemId: PropTypes.string.isRequired,
+    item_id: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
